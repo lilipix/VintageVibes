0d0deb5eeebb708c3a7bf09d81da9897db7e1f03
Aurélie <aurelie.demure.ad@gmail.com>
2023-10-09 22:27:47 +0200

    added favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { useEffect } from "react";
 import "./App.css";
 import AudioPlayer from "./Components/AudioPlayer";
 
 function App() {
-  useEffect(() => {
-    if ("screen" in window && "orientation" in window.screen) {
-      window.screen.orientation.lock("portrait").catch(function (error) {
-        console.error("Erreur lors du verrouillage de l'orientation: ", error);
-      });
-    }
-  }, []);
-
   return (
     <div className="bg-picture bg-cover bg-repeat h-screen w-screen ">
       <div className="rotate-container ">
